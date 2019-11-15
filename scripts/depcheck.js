68ee674266cf57ab603503bc0398ba4da03dafa9
Stephen Sorensen <shuoink@gmail.com>
2019-11-15 08:41:28 -0500

    depcheck improvments, fix deps

--- scripts/depcheck.js
+++ scripts/depcheck.js
@@ -1,32 +1,92 @@
-const cp = require('child_process');
-const {promisify} = require('util');
 const getPackages = require('./getPackages');
-
-const exec = promisify(cp.exec);
+const depcheck = require('depcheck');
+const path = require('path');
+const chalk = require('chalk');
 
 const depcheckPkg = async ({name, location}) => {
-  console.log(`checking dependencies for ${name}`);
-  const header = `\n===================\n${name}\n===================`;
-  try {
-    await Promise.race([
-      exec(`../../../node_modules/.bin/depcheck`, {cwd: location}),
-      new Promise((resolve, reject) =>
-        setTimeout(
-          () =>
-            reject(new Error(`depcheck(${name}) timed out after 30 seconds`)),
-          30000,
-        ),
+  const header = `\n=============================\n${name}\n=============================`;
+  let timeoutId;
+  const {
+    dependencies,
+    devDependencies,
+    missing,
+    using,
+    invalidFiles,
+    invalidDirs,
+  } = await Promise.race([
+    new Promise(resolve =>
+      depcheck(
+        path.resolve(__dirname, '..', location),
+        {
+          ignoreDirs: [
+            // folder with these names will be ignored
+            'build',
+            'coverage',
+            'node_modules',
+          ],
+          // parsers: {
+          //   // the target parsers
+          //   '*.js': depcheck.parser.es6,
+          //   '*.jsx': depcheck.parser.jsx,
+          // },
+          specials: [
+            // the target special parsers
+            depcheck.special.bin,
+            depcheck.special.eslint,
+            depcheck.special.webpack,
+            depcheck.special.jest,
+          ],
+        },
+        resolve,
       ),
-    ]);
-    console.log(`${header}\ngood to go`);
-  } catch (error) {
+    ),
+    new Promise((resolve, reject) => {
+      timeoutId = setTimeout(
+        () => reject(new Error(`depcheck(${name}) timed out after 30 seconds`)),
+        30000,
+      );
+    }),
+  ]);
+  clearTimeout(timeoutId);
+  console.log(header);
+  if (Object.keys(using).length) {
+    console.log(chalk.green('Using:', '\n ', Object.keys(using).join('\n  ')));
+  }
+  if (
+    dependencies.length ||
+    devDependencies.length ||
+    missing.length ||
+    invalidFiles.length ||
+    invalidDirs.length
+  ) {
     process.exitCode = 1;
-    if (error.stdout) {
-      console.log(header);
-      console.log(error.stdout);
-      return;
+    if (dependencies.length) {
+      console.log(
+        chalk.red('Unused Dependencies:', '\n ', dependencies.join('\n  ')),
+      );
     }
-    throw error;
+    if (devDependencies.length) {
+      console.log(
+        chalk.red(
+          'Unused Dev Dependencies:',
+          '\n ',
+          devDependencies.join('\n  '),
+        ),
+      );
+    }
+    if (missing.length) {
+      console.log(
+        chalk.red('Missing Dependencies:', '\n ', missing.join('\n  ')),
+      );
+    }
+    if (invalidFiles.length) {
+      console.log(
+        chalk.red('Invalid Files:', '\n ', invalidFiles.join('\n  ')),
+      );
+    }
+    if (invalidDirs.length) {
+      console.log(chalk.red('Invalid Dirs:', '\n ', invalidDirs.join('\n  ')));
+    }
   }
 };
 
@@ -36,10 +96,13 @@ const depcheckAll = async () => {
 };
 
 depcheckAll().then(
-  () => console.log('success!'),
+  () => {
+    if (!process.exitCode) {
+      console.log(chalk.green('\n\nsuccess!'));
+    }
+  },
   error => {
     process.exitCode = 1;
-    console.error({error});
-    process.exit(1);
+    console.error(error.stack);
   },
 );
